fe0f7ae7f8d012398da446f71668bfad1b89c4d9
Dhiraj-Shivade <dhiraj.shivade1@gmail.com>
2024-05-20 03:28:58 +0530

    New build.sh file with added path

--- build/build.sh
+++ build/build.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-# #Login to DockerHub
+# Navigate to the build directory
+cd /var/lib/jenkins/workspace/capston-project/build
+
+# Login to DockerHub
 echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
 
-## Determine the branch name
-if [ "$GIT_BRANCH" == "origin/dev" ]; then
-    # Build the Docker image
-    docker build -t my-react-app .
+# Build the Docker image
+docker build -t my-react-app .
 
+# Determine the branch name
+if [ "$GIT_BRANCH" == "origin/dev" ]; then
     ## For dev branch
     docker tag my-react-app:latest $DOCKER_DEV_REPO:latest
     docker push $DOCKER_DEV_REPO:latest
@@ -15,9 +18,6 @@ if [ "$GIT_BRANCH" == "origin/dev" ]; then
 elif [ "$GIT_BRANCH" == "origin/main" ]; then
     # Check if the latest commit on main includes a merge from dev
     if git log --format=%B --grep="Merge branch 'dev'"; then
-        # Build the Docker image
-        docker build -t my-react-app .
-
         # For main branch (after merging dev into main)
         docker tag my-react-app:latest $DOCKER_PROD_REPO:latest
         docker push $DOCKER_PROD_REPO:latest
