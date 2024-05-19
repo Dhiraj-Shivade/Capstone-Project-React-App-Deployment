1f9dadaf017976af5d01aba87e8c5a83aadf88c4
Dhiraj-Shivade <dhiraj.shivade1@gmail.com>
2024-05-20 03:02:14 +0530

    Added web as a service to build

--- build/build.sh
+++ build/build.sh
@@ -6,7 +6,7 @@ echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-
 ## Determine the branch name
 if [ "$GIT_BRANCH" == "origin/dev" ]; then
     # Build the Docker image
-    docker-compose build my-react-app .
+    docker-compose build web
 
     ## For dev branch
     docker tag my-react-app:latest $DOCKER_DEV_REPO:latest
@@ -16,7 +16,7 @@ elif [ "$GIT_BRANCH" == "origin/main" ]; then
     # Check if the latest commit on main includes a merge from dev
     if git log --format=%B --grep="Merge branch 'dev'"; then
         # Build the Docker image
-        docker-compose build my-react-app .
+        docker-compose build web
 
         # For main branch (after merging dev into main)
         docker tag my-react-app:latest $DOCKER_PROD_REPO:latest
