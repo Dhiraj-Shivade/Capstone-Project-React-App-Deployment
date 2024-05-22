f3a8cd85e0b854cafef6e78cf389407187bae9f1
Dhiraj-Shivade <dhiraj.shivade1@gmail.com>
2024-05-22 19:32:11 +0530

    Final Update

--- build/deploy.sh
+++ build/deploy.sh
@@ -2,28 +2,49 @@
 
 # Determine the repo to pull the image from
 if [ "$DOCKER_REPO" == "dev" ]; then
-    REPO=$DOCKER_DEV_REPO
-elif [ "$DOCKER_REPO" == "prod" ]; then 
-    REPO=$DOCKER_PROD_REPO
-fi
 
 # Pull the image from the respective repository
-docker pull $REPO:v1
+docker pull $DOCKER_USERNAME/my-react-app:v1
 
 # Check if the image pull was successful.
 if [ $? -ne 0 ]; then
-    echo "Failed to pull the Docker image from $REPO"
+    echo "Failed to pull the Docker image from $DOCKER_REPO"
     exit 1
 fi
 
 # Run the container
-docker run -d -p 80:80 $REPO:v1 sleep 3600
+docker run -d -p 80:80 my-react-app:v1
 
 
 # Check if the container run was successful
 if [ $? -ne 0 ]; then
-    echo "Failed to run the Docker container from $REPO"
+    echo "Failed to run the Docker container from $DOCKER_REPO"
     exit 1
 fi
 
 echo "Deployment successful"
+
+elif [ "$DOCKER_REPO" == "prod" ]; then 
+
+# Pull the image from the respective repository
+docker pull $DOCKER_USERNAME/my-react-app:v1
+
+# Check if the image pull was successful.
+if [ $? -ne 0 ]; then
+    echo "Failed to pull the Docker image from $DOCKER_REPO"
+    exit 1
+fi
+
+# Run the container
+docker run -d -p 80:80 my-react-app:v1
+
+
+# Check if the container run was successful
+if [ $? -ne 0 ]; then
+    echo "Failed to run the Docker container from $DOCKER_REPO"
+    exit 1
+fi
+
+echo "Deployment successful"
+fi
+
